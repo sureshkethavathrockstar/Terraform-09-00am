02ee8e3a8293aa644ff24d624081064b46e32cfb
sureshkethavathrockstar <kethavathsuresh357@gmail.com>
2024-07-27 16:33:23 +0530

    Update ec2.tf

--- day-3-custom-nw/ec2.tf
+++ day-3-custom-nw/ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "dev" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
-    subnet_id = aws_subnet.dev.id
     associate_public_ip_address = true
  
     tags = {
